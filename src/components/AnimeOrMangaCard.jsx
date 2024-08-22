7244ab086b14d5a60d7d46688447d0b42a3d877c
dprmd <adipr671@gmail.com>
2024-08-22 22:53:56 +0700

    add react-intersection-observer

--- src/components/AnimeOrMangaCard.jsx
+++ src/components/AnimeOrMangaCard.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prop-types */
 
+import { useInView } from "react-intersection-observer";
 import { useNavigate } from "react-router-dom";
 
 export default function AnimeOrMangaCard({ mal_id, image, title, rank }) {
+  const { ref } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
   const navigate = useNavigate();
   return (
     <div
       className="max-w-[47%] max-h-[275px] min-w-[47%] min-h-[275px] my-2 hover:scale-105 duration-500 cursor-pointer flex flex-col rounded-lg overflow-hidden sm:max-w-[32%] sm:max-h-[300px] sm:min-w-[32%] sm:min-h-[300px] md:max-w-[24%] md:max-h-[300px] md:min-w-[24%] md:min-h-[300px] lg:max-w-[18%] lg:max-h-[300px] lg:min-w-[18%] lg:min-h-[300px]"
       onClick={() => navigate(`/detail/${mal_id}`)}
+      ref={ref}
     >
       <div className="w-full h-full overflow-hidden relative">
         {rank && (
@@ -16,7 +23,7 @@ export default function AnimeOrMangaCard({ mal_id, image, title, rank }) {
             {rank}
           </span>
         )}
-        <img src={image} alt="Anime Picture" />
+        <img src={image} alt="Anime Picture" loading="lazy" />
       </div>
       <div className="bg-grayDark p-2 text-center grow flex justify-center items-center">
         <span className="font-poppins font-bold text-primary">{title}</span>
